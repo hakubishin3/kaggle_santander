5d762c28e33f8bf5bf6e88bbd3d510b686093ce1
hakubishin3 <goudasp2001@yahoo.co.jp>
2018-09-03 02:22:44 +0900

    tmp

--- src/models/base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class Model(metaclass=ABCMeta):
-    @abstractmethod
-    def fit(self, x_train, y_train, x_valid, y_valid, params):
-        raise NotImplementedError
